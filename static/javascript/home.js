eecbc69f0be2a437911e88f757128d1c173f9ef8
Dwi Laras Setyadita <dwilaras.2022@student.uny.ac.id>
2024-11-13 16:35:58 +0700

    split file html and js

---
+++ static/javascript/home.js
@@ -0,0 +1,29 @@
+let currentIndex = 0;
+const slidesContainer = document.querySelector('.slides-container');
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slidesContainer.style.transform = 'translateX(-${index * 100}%)';
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlides(currentIndex);
+}
+
+document.getElementById('nextSlide').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlides(currentIndex);
+});
+document.getElementById('prevSlide').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide
+})
+
+setInterval(nextSlide, 5000);
+showSlide(currentIndex);
